73b5e861d50d8e02e16834511ec762a30f16be0b
Patrick van Overdijk <p.vanoverdijk@student.avans.nl>
2015-11-18 21:53:10 +0100

    TestLevel + unit test afgemaakt

--- GameSnipperSFML_Cpp14/TestLevelImporter.cpp
+++ GameSnipperSFML_Cpp14/TestLevelImporter.cpp
@@ -6,8 +6,10 @@
 TestLevelImporter::TestLevelImporter()
 {
 	LevelImporter* levelImporter = new LevelImporter();
-	UnitTest::Compare("TestLevelImporter, LevelImported", levelImporter->Import("./Resources/levels/Level_New.json"), true);
-	UnitTest::Compare("TestLevelImporter, LevelImported", levelImporter->PrepareMusic("level1.ogg"), true);
-	UnitTest::Compare("TestLevelImporter, LevelImported", levelImporter->tileSets.size(), 3);
-	//UnitTest::Compare("TestLevelImporter, LevelImported", , );
+	UnitTest::Compare("TestLevelImporter, LevelImported", levelImporter->Import("./Resources/levels/UnitTestLevel.json"), true);
+	UnitTest::Compare("TestLevelImporter, MusicPrepared", levelImporter->PrepareMusic("level1.ogg"), true);
+	levelImporter->Prepare();
+	UnitTest::Compare("TestLevelImporter, TileSets", levelImporter->tileSets.size(), 2);
+	UnitTest::Compare("TestLevelImporter, Tiles", levelImporter->tiles.size(), 640);
+	UnitTest::Compare("TestLevelImporter, GameObjects", levelImporter->game_objects.size(), 4);
 }
