3db8745689ba37926b1bc8733a092f3e6e03c0a8
AndyScherrenberg <AndyScherrenberg@Gmail.com>
2015-11-26 11:28:45 +0100

    Switches kunnen nu een image hebben. De switch word ook echt geupdate kwa tekenen. ! Mogelijkheid om extra properties aan swtiches te geven

--- GameSnipperSFML_Cpp14/InteractiveFactory.cpp
+++ GameSnipperSFML_Cpp14/InteractiveFactory.cpp
@@ -29,7 +29,7 @@ GameObject* InteractiveFactory::Create(std::map<std::string, std::string> proper
 
 GameObject* InteractiveFactory::CreateSwitch(std::map<std::string, std::string>& properties, DrawContainer* container, GameObjectContainer* gameObjectContainer) {
 	Game_Switch *obj = nullptr;
-	std::string imgurl = properties["image"];
+	std::string imgurl =properties["image"];
 	int x, y, widht, height;
 	x = std::stoi(properties["x"]);
 	y = std::stoi(properties["y"]);
